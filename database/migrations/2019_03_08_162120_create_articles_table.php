bab20b7ef4ea5136a5cdb777b65853eb6d183b9a
Faiq Allam <faiq.kaboel@gmail.com>
2019-03-09 01:45:48 +0700

    Add create_articles_table migration for articles table

---
+++ database/migrations/2019_03_08_162120_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->enum('category', ['Illness', 'Medications', 'Living Healthy', 'Family', 'Healthy']);
+            $table->integer('writer_id');
+            $table->string('title');
+            $table->mediumText('content');
+            $table->text('cover_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
