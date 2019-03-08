0b52789a1eb73252f481c1c42c8375fae3f8d019
Faiq Allam <faiq.kaboel@gmail.com>
2019-03-09 01:47:02 +0700

    Add create_hospitals_table migration for hospitals table

---
+++ database/migrations/2019_03_08_164701_create_hospitals_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHospitalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hospitals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('cover_images_id');
+            $table->integer('city_id');
+            $table->string('name');
+            $table->text('biography');
+            $table->string('address');
+            $table->text('medical_services');
+            $table->text('public_services');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hospitals');
+    }
+}
