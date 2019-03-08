932c58fcff6179be005c40b13c9834ca7c8eece6
Faiq Allam <faiq.kaboel@gmail.com>
2019-03-08 23:06:16 +0700

    Add create_doctors_table migration for doctors table

---
+++ database/migrations/2019_03_08_155305_create_doctors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('specialization_id');
+            $table->string('name');
+            $table->mediumText('bio');
+            $table->string('photo');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+}
