d7123de88d713e854bb2c54bebb6267b3a97eab2
Faiq Allam <faiq.kaboel@gmail.com>
2019-03-08 23:04:25 +0700

    Edit create_users_table migration. Add more columns for users bio and profile picture

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->mediumText('bio');
+            $table->string('photo');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
