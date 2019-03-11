1d6bcc68480b0d39ba0a0be8df492bcd2ad6e63d
Alfaza Satria <alfazasatria8@gmail.com>
2019-03-11 21:51:10 +0700

    Edit Footer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -2,5 +2,5 @@
 <footer class="container">
       <hr>
         {{-- <p class="float-right"><a href="#">Back to top</a></p> --}}
-        <p>©2019 SIFIKS, Inc. · <a href="https://flying-coders.github.io/" __blank>About Us</a></p>
+        <p>©2019 SIFIKS, Inc. · <a>Visit Us</a> <img src="{{ asset('storage/images/Flying_Coders.png') }}" class="img-fluid" width="100px"> </p>
       </footer>
