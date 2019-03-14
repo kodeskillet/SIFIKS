9011a5ec422e42433edc03e74117b7ee0f497037
AhmadSyifaur <ahmadsyifaur11@gmail.com>
2019-03-14 19:36:55 +0700

    create admin control panel , dokter page, kesehatan page, artikel page,  "add dokter" page

---
+++ resources/views/admin/penyakit.blade.php
@@ -0,0 +1,43 @@
+ <!-- Content Header (Page header) -->
+ @extends('admin.dashboard')
+ @section('content')
+ <section class="content-header">
+      <h1>
+        Penyakit
+        <small>Info seputar Penyakit</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="/administrator/"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Penyakit</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">Title</h3>
+
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                    title="Collapse">
+              <i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fa fa-times"></i></button>
+          </div>
+        </div>
+        <div class="box-body">
+          Start creating your amazing application!
+        </div>
+        <!-- /.box-body -->
+        <div class="box-footer">
+          Footer
+        </div>
+        <!-- /.box-footer-->
+      </div>
+      <!-- /.box -->
+
+    </section>
+    @endsection
