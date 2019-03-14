0a2c6bf499d990e5f828b403e679de27c6b2e142
rivofausta <rivofaustav.rv@gmail.com>
2019-03-14 20:08:36 +0700

    add route href formArtikel

--- resources/views/admin/kesehatan.blade.php
+++ resources/views/admin/kesehatan.blade.php
@@ -18,7 +18,7 @@
       <!-- Default box -->
       <div class="box">
         <div class="box-header with-border">
-        <a href="/administrator/dokter/tambah/" class="btn btn-success pull-right"><i class="fa fa-plus"></i>Tambah artikel</a>
+        <a href="/administrator/artikel/tambah/" class="btn btn-success pull-right"><i class="fa fa-plus"></i>Tambah artikel</a>
           <!-- <div class="box-tools pull-right">
             <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
                     title="Collapse">
