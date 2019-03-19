6910c9db333f5c58e9d7ee4066b9e17ce45a6efe
Faiq Allam <faiq.kaboel@gmail.com>
2019-03-19 18:46:28 +0700

    Remove class unisharp/laravel-ckeditor

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        Unisharp\Ckeditor\ServiceProvider::class,
 
         /*
          * Package Service Providers...
