f7f665bf3baaf15afa59cb7a268d020ec9a9dc4c
Faiq Allam <faiq.kaboel@gmail.com>
2019-03-21 06:08:10 +0700

    Configure authentication redirection for each user roles

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,9 +17,24 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+        switch($guard) {
+            case 'admin':
+                if(Auth::guard($guard)->check()) {
+                    return redirect()->route('admin-index');
+                }
+                break;
+            case 'doctor':
+                if(Auth::guard($guard)->check()) {
+                    return redirect()->route('admin-index');
+                }
+                break;
+            default:
+                if (Auth::guard($guard)->check()) {
+                     return redirect('/home');
+                }
         }
+
+
 
         return $next($request);
     }
--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'biography', 'profile_picture', 'email', 'password'
     ];
 
     /**
