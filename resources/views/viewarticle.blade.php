852092efabfacba95e3befb5aac38de8ed28a0a2
rivofausta <rivofaustav.rv@gmail.com>
2019-03-21 17:46:15 +0700

    view article first model

---
+++ resources/views/viewarticle.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('content1')
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+        <article>
+            <h1>Breaking News: Cat does not give a damn</h1>
+            <p><strong>Author:</strong> Bill Clinton, <strong>Published:</strong> Yesterday</p>
+
+            <div>
+                <img src="#">
+                <span>Image source: The WEB</span>
+            </div>
+
+            <p>
+                This paragraph will include the photo of a random cat that the author of this tutorial has found online. You can replace this text with any random text. The sole purpose of this copy is to fill the space and make sure that the image to the left has enough text to low around it. I even decided to copy the previous paragraph. This paragraph will include the photo of a random cat that the author of this tutorial has found online. You can replace this text with any random text. The sole purpose of this copy is to fill the space and make sure that the image to the left has enough text to low around it.
+            </p>
+            <p>This paragraph will include the photo of a random cat that the author of this tutorial has found online. You can replace this text with any random text. The sole purpose of this copy is to fill the space and make sure that the image to the left has enough text fo low around it.</p>
+
+        </article>
+        </div>
+    </div>
+</div>
+@endsection
