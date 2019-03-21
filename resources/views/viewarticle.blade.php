e84490e621232336ffc554c4f4aec5f90ac32f0e
rivofausta <rivofaustav.rv@gmail.com>
2019-03-21 18:06:45 +0700

    edit p author for view article

--- resources/views/viewarticle.blade.php
+++ resources/views/viewarticle.blade.php
@@ -7,7 +7,7 @@
 		<div class="col-sm-12">
         <article>
             <h1>Breaking News: Cat does not give a damn</h1>
-            <p><strong>Author:</strong> Bill Clinton, <strong>Published:</strong> Yesterday</p>
+            
 
             <div>
                 <img src="#">
@@ -17,7 +17,14 @@
             <p>
                 This paragraph will include the photo of a random cat that the author of this tutorial has found online. You can replace this text with any random text. The sole purpose of this copy is to fill the space and make sure that the image to the left has enough text to low around it. I even decided to copy the previous paragraph. This paragraph will include the photo of a random cat that the author of this tutorial has found online. You can replace this text with any random text. The sole purpose of this copy is to fill the space and make sure that the image to the left has enough text to low around it.
             </p>
-            <p>This paragraph will include the photo of a random cat that the author of this tutorial has found online. You can replace this text with any random text. The sole purpose of this copy is to fill the space and make sure that the image to the left has enough text fo low around it.</p>
+            
+            <p>
+                This paragraph will include the photo of a random cat that the author of this tutorial has found online. You can replace this text with any random text. The sole purpose of this copy is to fill the space and make sure that the image to the left has enough text fo low around it.
+            </p>
+
+            <p>
+                <strong>Author:</strong> Bill Clinton, <strong>Published:</strong> Yesterday
+            </p>
 
         </article>
         </div>
