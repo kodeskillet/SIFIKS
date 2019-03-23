3dabd7a03840d1a9241a151fa34bc6a9aeeb3d6a
Faiq Allam <faiq.kaboel@gmail.com>
2019-03-23 21:50:48 +0700

    Configure facebook and google OAuth configuration data

--- config/services.php
+++ config/services.php
@@ -44,4 +44,24 @@ return [
         ],
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | laravel/socialite
+    |--------------------------------------------------------------------------
+    */
+
+    'facebook' => [
+        'client_id' => env('FB_CLIENT_ID'),
+        'client_secret' => env('FB_CLIENT_SECRET'),
+        'redirect' => env('FB_CLIENT_URL'),
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_CLIENT_URL'),
+    ],
+
+
+
 ];
