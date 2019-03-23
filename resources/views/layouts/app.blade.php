11fab6db446c52eba36b33c731d482a1c850b065
Faiq Allam <faiq.kaboel@gmail.com>
2019-03-23 21:51:37 +0700

    Import font-awesome from bower_components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <title>SIFIKS</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset("bower_components/font-awesome/css/font-awesome.min.css") }}">
 </head>
 <body>
 
