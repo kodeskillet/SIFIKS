ccfe5d5c3044207b95b897eb36755532cdf33420
FauzanAr <faurinta12345@gmail.com>
2019-03-23 15:02:39 +0700

    giving error condition to add admin form

--- resources/views/pages/ext/add-admin.blade.php
+++ resources/views/pages/ext/add-admin.blade.php
@@ -23,26 +23,46 @@
             {!! Form::open(['action' => 'AdminController@store','method'=> 'POST', 'enctype' => 'multipart/data']) !!}
             <div class="form-group row">
                 {{Form::label ('name','Username',['class'=>'col-md-2 col-form-label text-md-right'])}}
-                <div class="col-md-6">
-                        {{Form::text ('name','',['class'=>'form-control','placeholder'=>'Masukkan Username'])}}
+                <div class="col-md-4">
+                        {{Form::text ('name','',['class'=>'form-control float-right','placeholder'=>'Masukkan Username'])}}
+                        @if($errors->has('name'))
+                            <div class="text-danger">
+                                {{$errors->first('name')}}
+                            </div>
+                        @endif
                 </div>
             </div>
             <div class="form-group row">
                 {{Form::label ('email','E-Mail',['class'=>'col-md-2 col-form-label text-md-right'])}}
                 <div class="col-md-6">
                         {{Form::email ('email','',['class'=>'form-control','placeholder'=>'Masukkan Email'])}}
+                        @if($errors->has('email'))
+                            <div class="text-danger">
+                                {{$errors->first('email')}}
+                            </div>
+                        @endif
                 </div>
             </div>
             <div class="form-group row">
                 {{Form::label ('password','Password',['class'=>'col-md-2 col-form-label text-md-right'])}}
                 <div class="col-md-6">
-                        {{Form::password ('password',['class'=>'form-control','placeholder'=>'**********'])}}
+                    {{Form::password ('password',['class'=>'form-control','placeholder'=>'**********'])}}
+                        @if($errors->has('password'))
+                            <div class="text-danger">
+                                {{$errors->first('password')}}
+                            </div>
+                        @endif
                 </div>
             </div>
             <div class="form-group row">
                 {{Form::label ('password_confirmation','Ulangi Password',['class'=>'col-md-2 col-form-label text-md-right'])}}
                 <div class="col-md-6">
-                        {{Form::password ('password_confirmation',['class'=>'form-control','placeholder'=>'**********'])}}
+                    {{Form::password ('password_confirmation',['class'=>'form-control','placeholder'=>'**********'])}}
+                        @if($errors->has('password_confirmation'))
+                            <div class="text-danger">
+                                {{$errors->first('password_confirmation')}}
+                            </div>
+                        @endif
                 </div>
             </div>
             {{Form::submit('Add',['class'=>'btn btn-primary'])}}
