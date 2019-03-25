245d3156e8dc979cab62fd545e0898fc19910409
Faiq Allam <faiq.kaboel@gmail.com>
2019-03-25 10:46:37 +0700

    Add migration for adding gender to admins table

---
+++ database/migrations/2019_03_25_104509_add_gender_to_admins.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddGenderToAdmins extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('admins', function (Blueprint $table) {
+            $table->enum('gender', ['Laki - laki', 'Perempuan'])->after('name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('admins', function (Blueprint $table) {
+            $table->dropColumn('gender');
+        });
+    }
+}
