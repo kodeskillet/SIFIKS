5e0bb5de8177d70e56899cfd83e8b3c99fdb9d89
Faiq Allam <faiq.kaboel@gmail.com>
2019-03-26 03:34:38 +0700

    Add one to many relationship between doctor and article

--- app/Doctor.php
+++ app/Doctor.php
@@ -38,4 +38,12 @@ class Doctor extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function article() {
+        return $this->hasMany('App\Article');
+    }
+
+    public function detail() {
+        return $this->hasMany('App\DoctorDetail');
+    }
 }
