079c381c235066f235fe8cc76fa6483af35b5c44
Faiq Allam <faiq.kaboel@gmail.com>
2019-03-26 03:34:14 +0700

    Add one to many relationship between admin and article

--- app/Articles.php
+++ app/Articles.php
@@ -9,4 +9,12 @@ class Articles extends Model
     protected $table = 'articles';
     public $primaryKey = 'id';
     public $timestamps = true;
+
+    public function admin() {
+        return $this->belongsTo('App\Admin', 'writer_id');
+    }
+
+    public function doctor() {
+        return $this->belongsTo('App\Doctor', 'writer_id');
+    }
 }
