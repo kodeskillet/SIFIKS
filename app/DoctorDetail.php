96ecb54abed3dc6894280ae51da3f42a2cff12fa
Faiq <faiq.kaboel@gmail.com>
2019-03-26 17:05:35 +0700

    Add relationship between doctor_detail and hospital

--- app/DoctorDetail.php
+++ app/DoctorDetail.php
@@ -6,15 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class DoctorDetail extends Model
 {
-    protected $table = 'doctor_specializations';
+    protected $table = 'doctor_details';
     public $timestamps = true;
 
     public function doctor() {
         return $this->hasMany('App\Doctor', '');
     }
 
-    public function specialization() {
-        return $this->hasMany('App\DoctorSpecialization', 'specialization_id');
+    public function hospital() {
+        return $this->hasMany('App\Hospital', 'specialization_id');
     }
 
 }
