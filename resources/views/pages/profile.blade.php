a9782dc1d3fb3d07c4e52104b3217c5273b227e0
AhmadSyifaur <ahmadsyifaur11@gmail.com>
2019-03-26 16:53:57 +0700

    extends layout for admin profile page

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.adm-app')
 
 @section('content')
+<section class="content-header">
+        <h1>
+            Profile Admin
+            <small></small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> {{ session('role') }}</a></li>
+            <li class="active">QNA</li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
     
 @endsection
