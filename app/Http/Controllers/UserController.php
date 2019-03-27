53c3e54bb8604c9019ddadee1485acbb116cf08d
FauzanAr <faurinta12345@gmail.com>
2019-03-27 20:39:46 +0700

    Passing data to home

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Articles;
 
 class UserController extends Controller
 {
@@ -23,6 +24,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $article = Articles::OrderBy('created_at','asc')->take(5);
+        return view('home')->with('article', $article);
     }
 }
