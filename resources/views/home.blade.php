889cc54754c15c6b6bd03c8ca68ad33392d5f157
FauzanAr <faurinta12345@gmail.com>
2019-03-27 22:03:50 +0700

    Displaying done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,7 +44,7 @@
                 </div>
             </div>
             @endforeach
-            <div class="col-md-4">
+            {{-- <div class="col-md-4">
                 <div class="card mb-4 shadow-sm">
                     <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Image</text></svg>
                     <div class="card-body">
@@ -86,6 +86,6 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 @endsection
