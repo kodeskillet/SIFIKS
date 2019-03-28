27a1d46684dc5019befc14568b2eb67ca1454824
rivofausta <rivofaustav.rv@gmail.com>
2019-03-28 16:11:56 +0700

    add card doctor

--- resources/views/viewDoctor.blade.php
+++ resources/views/viewDoctor.blade.php
@@ -14,10 +14,18 @@
 
         <div class="col-md-4">
 
-
-            <h1 class="font-weight-bold">Test</h1>
-
-       
+        <div class="card" style="width: 18rem;">
+                    <br> <br>
+                    <img src="{{ asset('storage/images/namira.jpg') }}" class="card-user-profile-photo " alt="...">
+                    
+                    <div class="card-body card-user-profile-inner">
+                      
+                      <h5 class="text-center">dr. Namira Atasya, Sp.PD</h5>
+                      <a href="#" class="text-decoration-none">Dokter Penyakit Dalam</a>
+                      <br><br><br>
+                      <p class="font-weight-bold">Tindakan Medis : </p>
+                    </div>
+                  </div>       
 
         </div>
 
