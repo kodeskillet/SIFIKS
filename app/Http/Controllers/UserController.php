01749a2568b2088b2c24da60ad310ec573c66406
FauzanAr <faurinta12345@gmail.com>
2019-03-28 14:42:45 +0700

    creating edit function for user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
@@ -24,5 +25,10 @@ class UserController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function edit($id){
+        $user = User::find($id);
+        return view ('EditUser')->with($user);
     }
 }
