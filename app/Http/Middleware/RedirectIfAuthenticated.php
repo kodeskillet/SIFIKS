7562bfda89e991790e19632e9d1f11f63d9dd1b6
FauzanAr <faurinta12345@gmail.com>
2019-03-28 21:38:13 +0700

    return redirect auth ketinggalan gan

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -30,7 +30,7 @@ class RedirectIfAuthenticated
                 break;
             default:
                 if (Auth::guard($guard)->check()) {
-                     return redirect('');
+                     return redirect('/home');
                 }
         }
 
