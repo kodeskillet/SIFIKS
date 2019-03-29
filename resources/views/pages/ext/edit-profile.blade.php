2ac7f2563920f40141f8e4d7897ef29423e6003b
AhmadSyifaur <ahmadsyifaur11@gmail.com>
2019-03-29 09:18:06 +0700

    add input name , and email

--- resources/views/pages/ext/edit-profile.blade.php
+++ resources/views/pages/ext/edit-profile.blade.php
@@ -6,7 +6,17 @@
         <!-- <div class="container"> -->
             <p><h3>Edit Profile</h3></p>
             <div class="row">
-                
+                <div class="col-md-5">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                        <input type="text" class="form-control" placeholder="Name">
+                    </div>
+                    <br>
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                        <input type="email" class="form-control" placeholder="Email">
+                    </div>
+                </div>
             </div>
             <hr>
             <a href="" class="btn btn-primary">Save</a>
