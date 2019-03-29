ae44580df19d49cc7dcb8ceaa990495ca42fe6f3
AhmadSyifaur <ahmadsyifaur11@gmail.com>
2019-03-29 09:31:08 +0700

    create modal edit pictre

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -22,7 +22,35 @@
                 <h3 class="profile-username text-center">AhmadSyifaur</h3>
                 <p class="text-muted text-center">ahmadsyifaur11@gmail.com</p>
 
-                <a href=""><p class="text text-center">Change Picture</p></a>
+                <a href="" data-toggle="modal" data-target="#modal-default"><p class="text text-center">Change Picture</p></a>
+                <!-- <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-default">
+                  Change Picture
+                </button> -->
+                <!-- Modal -->
+                <div class="modal fade" id="modal-default" style="display: none;">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">×</span></button>
+                        <h4 class="modal-title">Change Picture</h4>
+                      </div>
+                      <div class="modal-body">
+                        <img class="center" src="/bower_components/admin-lte/dist/img/user4-128x128.jpg" alt="User profile picture">
+                        <div class="form-group">
+                          <label for="exampleInputFile">Import From PC</label>
+                          <input type="file" id="exampleInputFile">
+                        </div>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                      </div>
+                    </div>
+                    <!-- /.modal-content -->
+                  </div>
+                  <!-- /.modal-dialog -->
+                </div>
                 <hr>
                 <ul class="list-group list-group-unbordered sidebar-menu tree" data-widget="tree">
                     <li>
