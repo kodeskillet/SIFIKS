afc674cebfa05c42b9715f82034ed115237d082b
Faiq Allam <faiq.kaboel@gmail.com>
2019-04-01 18:57:33 +0700

    Add adm-edit-password modal for Admin Edit passwords

---
+++ resources/views/pages/ext/modal/adm-edit-password.blade.php
@@ -0,0 +1,22 @@
+<div class="modal fade" id="editPassword" style="display: none;">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span></button>
+                <h4 class="modal-title">Change Picture</h4>
+            </div>
+            <div class="modal-body">
+                <img class="center" src="/bower_components/admin-lte/dist/img/user4-128x128.jpg" alt="User profile picture">
+                <div class="form-group">
+                    <label for="exampleInputFile">Import From PC</label>
+                    <input type="file" id="exampleInputFile">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
