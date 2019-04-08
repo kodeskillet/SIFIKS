067b3caa093b446dc4522a67b3726281f739cbc2
Faiq Allam <faiq.kaboel@gmail.com>
2019-04-09 02:19:48 +0700

    Implement facebook auth

--- resources/views/layouts/inc/oauth.blade.php
+++ resources/views/layouts/inc/oauth.blade.php
@@ -1,13 +1,13 @@
 <div class="row justify-content-center mt-3">
     <div class="col-md-6">
         <div class="btn-group btn-block">
-            <a href="{{ route('api.login', ['provider' => 'google']) }}" class="btn btn-soc btn-google btn-lg p-3" title="Google">
+            <a href="{{ route('api.login', ['provider' => 'google']) }}" class="btn btn-soc btn-google btn-lg p-3" title="Masuk dengan Google">
                 <i class="fab fa-google fa-lg"></i>
             </a>
-            <a href="{{ route('api.login', ['provider' => 'twitter']) }}" class="btn btn-soc btn-twitter btn-lg p-3" title="Twitter">
+            <a href="{{ route('api.login', ['provider' => 'twitter']) }}" class="btn btn-soc btn-twitter btn-lg p-3" title="Masuk dengan Twitter">
                 <i class="fab fa-twitter fa-lg"></i>
             </a>
-            <a href="{{ route('api.login', ['provider' => 'facebook']) }}" class="btn btn-soc btn-facebook btn-lg p-3" title="Facebook">
+            <a href="{{ route('api.login', ['provider' => 'facebook']) }}" class="btn btn-soc btn-facebook btn-lg p-3" title="Masuk dengan Facebook">
                 <i class="fab fa-facebook fa-lg"></i>
             </a>
         </div>
