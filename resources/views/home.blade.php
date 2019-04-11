76e5428c140f4a11912580adebb64c50d49155d6
Alfaza Satria <alfazasatria8@gmail.com>
2019-04-11 10:38:47 +0700

    mbuhkah

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,7 +29,7 @@
                     </div>
                 </div>
                 <div class="col-md-3">
-                    <img src="{{ asset('storage/images/dokterhome.png') }}" alt="Dokter" class="float-right thumbnail-fluid" width="150%" >
+                    <img src="{{ asset('storage/images/dokterhome.png') }}" alt="Dokter" class="float-right rounded-0" width="150%" >
                 </div>
             </div>
         </div>
