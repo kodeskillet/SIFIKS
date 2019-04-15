9d3f1a2967514f2ed5027fcdfed3e8563d0efbdd
Faiq Allam <faiq.kaboel@gmail.com>
2019-04-16 01:58:36 +0700

    Add Thread Model with Relationship stating that Thread belongsTo User and Topic

---
+++ app/Thread.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Thread extends Model
+{
+    protected $table = 'threads';
+
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+
+    public function topic() {
+        return $this->belongsTo('App\ThreadTopic', 'id_topic');
+    }
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    public function trimStr(string $string) {
+        if(strlen($string) > 200) {
+            return substr($string, 0, 200).'...';
+        }
+    }
+}
