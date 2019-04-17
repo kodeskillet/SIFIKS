6f6af9ce8566d312616eab298e1b74e7c1105f7c
Faiq Allam <faiq.kaboel@gmail.com>
2019-04-18 00:21:53 +0700

    Give access only to doctors for ThreadAnswerController

--- app/Http/Controllers/ThreadAnswerController.php
+++ app/Http/Controllers/ThreadAnswerController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class ThreadAnswerController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:doctor');
+    }
+
     /**
      * Display a listing of the resource.
      *
