b961558d2c69da0aa988e5a50febe7d4f1631e9c
Faiq Allam <faiq.kaboel@gmail.com>
2019-04-19 17:11:14 +0700

    Add access controls based on each user roles

--- resources/views/pages/thread.blade.php
+++ resources/views/pages/thread.blade.php
@@ -30,7 +30,7 @@
                                         href="{{ route('doctor.thread.index', ['query' => 'all']) }}"
                                     @endif
                                 >
-                                    <i class="fa fa-inbox text-blue"></i>
+                                    <i class="fa fa-comments text-blue"></i>
                                     Semua
                                     <span class="label label-primary pull-right">
                                         {{ $data['count']['all'] }}
@@ -74,11 +74,13 @@
             </div>
 
             <div class="col-md-9">
+
+                @include('layouts.inc.messages')
+
                 <div class="box box-primary">
                     <div class="box-header with-border">
                         <h3 class="box-title">
                             Pertanyaan
-
                         </h3>
 
                         <div class="box-tools pull-right">
@@ -102,19 +104,26 @@
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body no-padding">
-                        <div class="table-responsive mailbox-messages">
-                            <table class="table table-hover table-striped">
-                                <tbody>
+                        @if(count($data['threads']) > 0)
+                            <div class="table-responsive mailbox-messages">
+                                <table class="table table-hover table-striped">
+                                    <tbody>
                                     @foreach($data['threads'] as $thread)
-                                        <tr
+                                        <tr data-toggle="tooltip"
                                             @if($thread->status == true)
-                                                @if($thread->doctor_id == Auth::guard('doctor')->user()->id)
-                                                    title="Terjawab oleh anda"
-                                                @else
-                                                    title="Terjawab oleh dr. {{ $thread->doctor->name }}"
-                                                @endif
+                                            @auth('admin')
+                                            title="Terjawab oleh dr. {{ $thread->doctor->name }}"
+                                            @endauth
+
+                                            @auth('doctor')
+                                            @if($thread->doctor_id == Auth::guard('doctor')->user()->id)
+                                            title="Terjawab oleh anda"
                                             @else
-                                                title="Belum Terjawab"
+                                            title="Terjawab oleh dr. {{ $thread->doctor->name }}"
+                                            @endif
+                                            @endauth
+                                            @else
+                                            title="Belum Terjawab"
                                             @endif
                                         >
                                             <td class="mailbox-star">
@@ -128,28 +137,58 @@
                                                 {{ Str::limit($thread->user->name, 15) }}
                                             </td>
                                             <td class="mailbox-subject">
-                                                <b>{{ Str::limit($thread->topic->topic_name, 20) }}</b>
+                                                <b>{{ Str::limit($thread->topic->topic_name, 30) }}</b>
                                             </td>
                                             <td>
                                                 <span>{!! Str::limit($thread->question, 25) !!}</span>
                                             </td>
                                             <td class="mailbox-attachment">
-                                                <a href="#" class="btn btn-success btn-xs {{ $thread->status == true ? 'disabled' : '' }}" style="{{ $thread->doctor_id == Auth::guard('doctor')->user()->id ? 'display: none;' : ''}}">
-                                                    Jawab
+                                                <a href="{{ route(session('guard').'.thread.show', $thread->id) }}" class="btn btn-info btn-xs" title="Selengkapnya" data-toggle="tooltip">
+                                                    <i class="fas fa-external-link-square-alt"></i>
                                                 </a>
-                                                @if($thread->doctor_id == Auth::guard('doctor')->user()->id)
-                                                    <a href="#" class="btn btn-warning btn-xs">
-                                                        Ubah Jawaban
+                                                @auth('admin')
+                                                    <a href="#" class="btn btn-danger btn-xs" title="Hapus">
+                                                        <i class="fas fa-trash-alt"></i>
                                                     </a>
-                                                @endif
+                                                @endauth
+
+                                                @auth('doctor')
+                                                    <a href="#" class="btn btn-success btn-xs {{ $thread->status == true ? 'disabled' : '' }}" style="{{ $thread->doctor_id == Auth::guard('doctor')->user()->id ? 'display: none;' : ''}}" title="Jawab" data-toggle="tooltip">
+                                                        <i class="fas fa-reply"></i>
+                                                    </a>
+                                                    @if($thread->doctor_id == Auth::guard('doctor')->user()->id)
+                                                        <a href="#" class="btn btn-warning btn-xs" title="Edit Jawaban" data-toggle="tooltip">
+                                                            <i class="fas fa-sync-alt"></i>
+                                                        </a>
+                                                        <a href="#" onclick="$('#delAns').submit()" class="btn btn-danger btn-xs" title="Hapus Jawaban" data-toggle="tooltip">
+                                                            <i class="fas fa-trash"></i>
+                                                        </a>
+
+                                                        <form onsubmit="return confirm('Yakin ingin hapus jawaban?')" id="delAns" action="{{ route('doctor.thread.answer.destroy', $thread->id) }}" method="POST">
+                                                            @csrf
+                                                            <input type="hidden" name="_method" value="PUT">
+                                                        </form>
+                                                    @endif
+                                                @endauth
                                             </td>
                                             <td class="mailbox-date">{{ $thread->created_at->diffForHumans() }}</td>
                                         </tr>
                                     @endforeach
-                                </tbody>
-                            </table>
-
-                        </div>
+                                    </tbody>
+                                </table>
+                            </div>
+                        @else
+                            <br>
+                            <div class="alert alert-warning col-md-8 col-md-offset-2 text-center text-bold">
+                                @if($data['query'] == "all")
+                                    Belum ada diskusi apapun.
+                                @elseif($data['query'] = "answered")
+                                    Belum ada diskusi yang terjawab.
+                                @else
+                                    Semua diskusi telah terjawab.
+                                @endif
+                            </div>
+                        @endif
                     </div>
                 </div>
                 <div class="text-center">
