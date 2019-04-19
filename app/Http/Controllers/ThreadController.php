5989a1a747a2a7001c647df4a5571962c4b1ca92
Faiq Allam <faiq.kaboel@gmail.com>
2019-04-19 17:08:15 +0700

    Add show function to be excepted

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -11,7 +11,7 @@ class ThreadController extends Controller
     public function __construct()
     {
         $this->middleware('auth:admin', ['except' => [
-            'index'
+            'index', 'show'
         ]]);
     }
 
