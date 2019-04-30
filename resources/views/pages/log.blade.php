1c31e3326cd6434d97ce89252cf2253167062aed
Faiq Allam <faiq.kaboel@gmail.com>
2019-04-30 10:43:24 +0700

    Fix layouts for each log targets and prefixes

--- resources/views/pages/log.blade.php
+++ resources/views/pages/log.blade.php
@@ -6,111 +6,106 @@
             <strong class="box-title">Log Aktivitas</strong>
         </div>
         <div class="box-body bg-gray-light">
+
+            @if(count($data['logs']) > 0)
+
             <ul class="timeline">
 
-                <li class="time-label">
-                    <span class="bg-aqua">
-                        26 Apr. 2019
+                @foreach($data['logs'] as $log)
+                    <li class="time-label">
+                    <span class="bg-navy">
+                        {{ $log->created_at->format('d F Y') }}
+                        &nbsp;-&nbsp;
+                        <i class="far fa-clock"></i>
+                        {{ $log->created_at->format('G:i') }}
                     </span>
-                </li>
-                <li>
-                    <!-- timeline icon -->
-                    <i class="fa fas fa-user-edit bg-blue"></i>
-                    <div class="timeline-item">
-                        <span class="time"><i class="far fa-clock"></i> 00:05</span>
+                    </li>
+                    <li>
+                        <!-- timeline icon -->
+                        @if($log->prefix == 'a-create')
+                            <i class="fa fas fa-pen bg-blue"></i>
+                        @elseif($log->prefix == 'a-update')
+                            <i class="fa fas fa-user-edit bg-orange"></i>
+                        @elseif($log->prefix == 't-create')
+                            <i class="fa fas fa-comment-dots bg-yellow"></i>
+                        @elseif($log->prefix == 't-answer')
+                            <i class="fa fas fa-reply-all bg-green"></i>
+                        @else
+                        @endif
+                        <div class="timeline-item">
+                            <span class="time"><i class="far fa-clock"></i> {{ $log->created_at->diffForHumans() }}</span>
 
-                        <h3 class="timeline-header">
-                            <span class="badge bg-red">Admin</span>&nbsp;
-                            <a href="#">Fauzan AR.</a> membuat artikel baru.
-                        </h3>
+                            <h3 class="timeline-header">
+                                @if($log->admin_id != null)
+                                    <span class="badge bg-red">
+                                        Admin
+                                    </span>
+                                    <a href="#">{{ $log->admin->name }}</a>
+                                @elseif($log->doctor_id != null)
+                                    <span class="badge bg-yellow">
+                                        Doctor
+                                    </span>
+                                    <a href="#">{{ $log->doctor->name }}</a>
+                                @else
+                                    <span class="badge bg-blue">
+                                        Member
+                                    </span>
+                                    <a href="#">{{ $log->member->name }}</a>
+                                @endif
+                                {{ $log->action }}
+                                @if($log->prefix == "t-answer")
+                                    <a href="#">{{ $log->thread->user->name }}</a>
+                                @endif
+                            </h3>
 
-                        <div class="timeline-body">
-                            <h4>Ablasi Retina</h4>
-                            <p>Ablasi retina adalah penyakit mata akibat lepasnya lapisan tipis di dalam mata yang disebut retina. Kondisi ini tergolong darurat dan dapat menyebabkan kebutaan perma...</p>
+                            <div class="timeline-body">
+                                @if($log->target == "article")
+                                    <h4>
+                                        {{ $log->article->title }}
+                                    </h4>
+                                    <p>
+                                        {!! \Illuminate\Support\Str::limit($log->article->content, 200) !!}
+                                    </p>
+                                @else
+                                    @if($log->prefix == "t-create")
+                                        <h4>
+                                            {{ $log->thread->topic->topic_name }}
+                                        </h4>
+                                        <p>
+                                            {!! \Illuminate\Support\Str::limit($log->thread->question, 200) !!}
+                                        </p>
+                                    @elseif($log->prefix == "t-answer")
+                                        <p>
+                                            {!! \Illuminate\Support\Str::limit($log->thread->answer, 200) !!}
+                                        </p>
+                                    @endif
+                                @endif
+                            </div>
+
+                            <div class="timeline-footer">
+                                <a class="btn btn-primary btn-xs" href="
+                                    @if($log->target == "article")
+                                        {{ url('/admin/article/'.$log->article->id) }}
+                                    @else
+                                        {{ url('/admin/thread/'.$log->thread->id.'/show') }}
+                                    @endif
+                                ">
+                                    <i class="fa fa-share margin-r-5"></i>Selengkapnya
+                                </a>
+                            </div>
                         </div>
-
-                        <div class="timeline-footer">
-                            <a class="btn btn-primary btn-xs" href="#">
-                                <i class="fa fa-share margin-r-5"></i>Selengkapnya
-                            </a>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <!-- timeline icon -->
-                    <i class="fa fas fa-reply-all bg-green"></i>
-                    <div class="timeline-item">
-                        <span class="time"><i class="far fa-clock"></i> 15:34</span>
-
-                        <h3 class="timeline-header">
-                            <span class="badge bg-yellow-gradient">Dokter</span>&nbsp;
-                            <a href="#">Alfuzzy Satria J.</a> menjawab diskusi oleh <a href="#">Ahmad Syifaur</a>.
-                        </h3>
-
-                        <div class="timeline-body">
-                            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium deleniti atque corrupti cumque nihil impedit quo minus id quod maxime...</p>
-                        </div>
-
-                        <div class="timeline-footer">
-                            <a class="btn btn-primary btn-xs" href="#">
-                                <i class="fa fa-share margin-r-5"></i>Selengkapnya
-                            </a>
-                        </div>
-                    </div>
-                </li>
-
-                <li class="time-label">
-                    <span class="bg-purple">
-                        25 Apr. 2019
-                    </span>
-                </li>
-                <li>
-                    <!-- timeline icon -->
-                    <i class="fa fas fa-reply-all bg-green"></i>
-                    <div class="timeline-item">
-                        <span class="time"><i class="far fa-clock"></i> 21:21</span>
-
-                        <h3 class="timeline-header">
-                            <span class="badge bg-yellow-gradient">Dokter</span>&nbsp;
-                            <a href="#">Alfuzzy Satria J.</a> menjawab diskusi oleh <a href="#">Endah Astuti</a>.
-                        </h3>
-
-                        <div class="timeline-body">
-                            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium deleniti atque corrupti cumque nihil impedit quo minus id quod maxime...</p>
-                        </div>
-
-                        <div class="timeline-footer">
-                            <a class="btn btn-primary btn-xs" href="#">
-                                <i class="fa fa-share margin-r-5"></i>Selengkapnya
-                            </a>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <!-- timeline icon -->
-                    <i class="fa fas fa-comment-dots bg-yellow"></i>
-                    <div class="timeline-item">
-                        <span class="time"><i class="far fa-clock"></i> 12:49</span>
-
-                        <h3 class="timeline-header">
-                            <span class="badge bg-blue-gradient">Member</span>&nbsp;
-                            <a href="#">Endah Astuti</a> membuat diskusi baru.
-                        </h3>
-
-                        <div class="timeline-body">
-                            <h4>Telinga saya budeg</h4>
-                            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium deleniti atque corrupti cumque nihil impedit quo minus id quod maxime...</p>
-                        </div>
-
-                        <div class="timeline-footer">
-                            <a class="btn btn-primary btn-xs" href="#">
-                                <i class="fa fa-share margin-r-5"></i>Selengkapnya
-                            </a>
-                        </div>
-                    </div>
-                </li>
+                    </li>
+                @endforeach
 
             </ul>
+            @else
+                <div class="alert alert-warning text-center">
+                    <strong>Belum ada aktivitas apapun.</strong>
+                </div>
+            @endif
         </div>
+    </div>
+    <div class="text-center">
+        {{ $data['logs']->links() }}
     </div>
 @endsection
