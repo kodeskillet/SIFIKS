99bb47edd98ef94e48f7514049406e13bc339fa4
Faiq Allam <faiq.kaboel@gmail.com>
2019-05-15 02:00:51 +0700

    Add social media sharing buttons

---
+++ resources/views/layouts/inc/sharer.blade.php
@@ -0,0 +1,9 @@
+<a href="https://twitter.com/intent/tweet?text=my share text&amp;url={{ url()->current() }}" class="btn-share share-twitter" target="_blank" title="Bagikan dengan Twitter">
+    <span class="fab fa-twitter"></span>
+</a>
+<a href="https://www.facebook.com/sharer/sharer.php?u={{ url()->current() }}" class="btn-share share-fb" target="_blank" title="Bagikan dengan Facebook">
+    <span class="fab fa-facebook"></span>
+</a>
+<a href="mailto:?subject=I SIFIKS&amp;body=Informasi Kesehatan {{ url()->current() }}" class="btn-share share-email" target="_blank" title="Bagikan dengan E-Mail">
+    <span class="fas fa-envelope"></span>
+</a>
--- resources/views/viewarticle.blade.php
+++ resources/views/viewarticle.blade.php
@@ -6,52 +6,70 @@
 
     @include('layouts.inc.navbar')
 
-<div class="container mt-4">
-		<div class="container">
-            <div class="row col-md-12">
-                <div class="col-md-6">
-                    <span class="badge
-                    @if($article->category == "penyakit")
-                        bg-red
-                    @elseif($article->category == "obat")
-                        bg-blue
-                    @elseif($article->category == "kesehatan")
-                        bg-orange
-                    @elseif($article->category == "hidup-sehat")
-                        bg-yellow
-                    @else
-                        bg-green
+    <div class="container mt-4 pl-5 pr-5">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-md-12">
+                        <span class="badge
+                        @if($article->category == "penyakit")
+                            bg-red
+                        @elseif($article->category == "obat")
+                            bg-blue
+                        @elseif($article->category == "kesehatan")
+                            bg-orange
+                        @elseif($article->category == "hidup-sehat")
+                            bg-yellow
+                        @else
+                            bg-green
+                        @endif
+                            ">
+                            {{ $article->getCat($article->category) }}
+                        </span>
+                        <h1 class="mt-2"> {{ $article->title }} </h1>
+                        <div class="row">
+                            <div class="col mb-2">
+                                @include('layouts.inc.sharer')
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center">
+                    @if ($article->cover_image != null && $article->cover_image != "noimage.jpg")
+                        <img class="center size-full wp-image-1544282" src="{{ asset('storage/cover_images/'.$article->cover_image) }}" alt="{{$article->title}}" width="650" height="433">
                     @endif
-                    ">
-                        {{ $article->getCat($article->category) }}
-                    </span>
-                    <h1 class="mt-2"> {{ $article->title }} </h1>
                 </div>
-            </div>
-            <div class="text-center">
-                @if ($article->cover_image != null)
-                    <img class="center size-full wp-image-1544282" src="{{ asset('storage/cover_images/').'/'.$article->cover_image}}" alt="{{$article->title}}" width="650" height="433">
-                @else
-                    <img class="alignnone size-full wp-image-1544282" src="{{ asset('storage/cover_images/noimage.jpg')}}" alt="{{$article->title}}" width="650" height="433">
-                @endif
-            </div>
-            <div class="row container">
-                <div class="col-md-12">
-                    {!! $article->content !!}
+                <div class="row">
+                    <div class="col-md-12">
+                        {!! $article->content !!}
+                    </div>
                 </div>
+                <div class="row mt-5">
+                    <div class="col-md-6">
+                        <p>
+                            Ditinjau oleh
+                            @if($article->admin_id == null)
+                                <strong>dr. {{ $article->doctor->name }} {{ $article->doctor->specialty->degree  }}</strong>
+                            @else
+                                <strong>Admin</strong>
+                            @endif
+                            <br>
+                            <small> <i class="fas fa-clock"></i> {{ $article->created_at->diffForHumans() }}</small>
+                        </p>
+                    </div>
+                    <div class="col text-right">
+                        @include('layouts.inc.sharer')
+                    </div>
+                </div>
+                <br><br><br>
             </div>
-            <p class="mt-5 mb-5">
-                <small>Ditinjau oleh </small>
-                @if($article->admin_id == null)
-                    <strong>dr. {{ $article->doctor->name }} {{ $article->doctor->specialty->degree  }}</strong>
-                @else
-                    <strong>Admin</strong>
-                @endif
+            <div class="col col-md-4">
                 <br>
-                <small> <i class="fas fa-clock"></i> {{ $article->created_at->diffForHumans() }}</small>
-            </p>
-            <br><br><br>
+                <br>
+                <a href="{{ route('user.thread.create') }}"><img src="{{ asset('storage/images/doctor.png') }}" alt="doctor" width="100%"></a>
+                <hr>
+                <img src="{{ asset('storage/images/health.png') }}" alt="health" width="100%">
+            </div>
         </div>
     </div>
-</div>
 @endsection
